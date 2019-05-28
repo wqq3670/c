278885f3a7276c5ab58edc9beaded5f47315c0e6
wqq3670 <1121093520@qq.com>
2019-05-28 21:48:48 +0800

    提交代码

---
+++ 5.28  hom/5.28  hom/源.cpp
@@ -0,0 +1,96 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	int d = 0;
+//	int e = 0;
+//	for (a = 1; a <= 5; a++)
+//	{
+//		for (b = 1;  b<= 5; b++)
+//		{
+//			for (c = 1; c <= 5; c++)
+//			{
+//				for (d = 1; d <= 5; d++)
+//				{
+//					for (e = 1; e <= 5; e++)
+//					{
+//						if (((b == 2) + (a == 3) == 1) &&
+//							((b == 2) + (e == 4) == 1) &&
+//							((c == 1) + (d == 2) == 1) &&
+//							((c == 5) + (d == 3) == 1) &&
+//							((e == 4) + (a == 1) == 1))
+//						{
+//							if(a*b*c*d*e == 120)
+//							printf("a=%d b=%d c=%d d=%d e=%d", a, b, c, d, e);
+//
+//						}
+//						   
+//					}
+//
+//				}
+//
+//			}
+//
+//		}
+//
+//	}
+//	system("pause");
+//	return 0;
+//}
+ 
+
+//int main()
+//{
+//	int killer = 0;
+//	for (killer = 'a'; killer <= 'd';killer++)
+//	{
+//		if ((killer != 'a') + (killer == 'c') + (killer == 'd') + (killer != 'd') == 3)
+//		{
+//			printf("killer=%c\n", killer);
+//		}
+//	}
+//
+//	system("pause");
+//	return 0;
+//}
+
+int main()
+{
+	int arr[10][10] = { 0 };
+	int i = 0;
+	for (i = 0; i < 10; i++)
+	{
+		int j = 0;
+		for (j = 0; j < 10; j++)
+		{
+			if (j == 0)
+			{
+				arr[i][j] = 1;
+			}
+			if (i==j)
+			{
+				arr[i][j] = 1;
+			}
+			if (i >= 2 && j >= 1)
+			{
+				arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
+			}
+		}
+	}
+	for (i = 0; i < 10; i++)
+	{
+		int j = 0;
+		for (j = 0; j <= i; j++)
+		{
+			printf("%d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+	system("pause");
+	return 0;
+}
